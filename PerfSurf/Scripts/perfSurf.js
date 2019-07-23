dcaf6044e3b56b8ac00baf8bda54b849dd3a36ec
Syune Ghazaryan <syune.ghazaryan@accenture.com>
2019-07-23 17:41:04 +0300

    init

---
+++ PerfSurf/Scripts/perfSurf.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    var perfHub = $.connection.perfHub;
+    $.connection.hub.loggin = true;
+    $.connection.hub.start();
+
+
+    perfHub.client.newMessage = function (message) {
+        model.addMessage(message);
+    }
+
+    var Model = function () {
+        var self = this;
+        self.message = ko.observable("");
+        self.messages = ko.observableArray();
+    }
+    //behaviour
+    Model.prototype = {
+        sendMessage: function () {
+            var self = this;
+            perfHub.server.send(self.message());
+            self.message("");
+        },
+        addMessage: function (message) {
+            var self = this;
+            self.message.push(message);
+
+        }
+
+    }
+    var model = new Model();
+
+    (function () {
+        ko.applyBindings(model);
+    }())
+
+}())
